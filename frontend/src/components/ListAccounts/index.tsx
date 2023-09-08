09701b534a957fb7e370d1669dfc9b903e522645
Igor Marinho <igormarinholeo2014@gmail.com>
2023-09-08 18:21:06 -0300

    Modal add to applications and accounts.

--- frontend/src/components/ListAccounts/index.tsx
+++ frontend/src/components/ListAccounts/index.tsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import Card from '@mui/material/Card';
 import Button from "@mui/material/Button";
 import styles from "./ListAccounts.module.scss";
- 
+
 
 import IAccount from "../../interfaces/IAccount";
-import { CardContent, Divider, Chip } from "@mui/material";
+import { CardContent, Divider, Chip, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 
 const url = 'https://moneyapp.onrender.com/account';
 
@@ -17,6 +17,16 @@ const ListAccount = () => {
 
   const [accounts, setAccounts] = useState<IAccount[]>([]);
 
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   useEffect(() => {
     axios.get(url)
       .then(response => {
@@ -26,7 +36,7 @@ const ListAccount = () => {
         console.log(err)
       })
 
-  }, [accounts]); 
+  }, [accounts]);
 
   return (
     <div>
@@ -36,40 +46,53 @@ const ListAccount = () => {
 
       <ul>
         {accounts.map(dado => (
-          <Card sx={{  minWidth: 275 }} color="text-secondary"  >
+          <Card sx={{ minWidth: 275 }} color="text-secondary"  >
             <CardContent>
               <div >
-                <li className={styles.main} key={dado._id as any}>  
-                <Chip label="ID Conta : " color="primary"  variant="outlined" />  
-     
-                 {dado._id }
+                <li className={styles.main} key={dado._id as any}>
+                  <Chip label="ID Conta : " color="primary" variant="outlined" />
+
+                  {dado._id}
                   <p key={dado.created_at} >
-                  <Chip label="Data de Criação : " color="success" variant="outlined" />  
-     
+                    <Chip label="Data de Criação : " color="success" variant="outlined" />
+
                     {dado.created_at}
-                    </p>
-               <p>
-                <Chip label="Agencia :" color="success" variant="outlined" /> {dado.agency} 
-                </p> 
+                  </p>
+                  <p>
+                    <Chip label="Agencia :" color="success" variant="outlined" /> {dado.agency}
+                  </p>
                   <p key={dado.account_number}>
-                  <Chip label="Conta :" color="success" variant="outlined" />  
-                     {dado.account_number}</p>
-                  <p key={dado.balance.$numberDecimal} >
-                  <Chip label="Saldo :" color="success" variant="outlined" />  
-                     {dado.balance.$numberDecimal}</p>
+                    <Chip label="Conta :" color="success" variant="outlined" />
+                    {dado.account_number}</p>
 
+                
                   <p key={dado.user_id} className={styles.linha}>
-                     <Chip label=" User ID : " color="primary" variant="outlined" />
-                    
-                    {dado.user_id}</p>
-                <li><Button color="warning" variant="contained">Editar</Button></li>
-                  <li><Button color="error" variant="contained">Excluir</Button></li>
+                    <Chip label=" User ID : " color="primary" variant="outlined" />
 
+                    {dado.user_id}</p>
+                  <li><Button color="warning" variant="contained" onClick={handleOpen}>Detalhes</Button></li>
+ 
 
                 </li>
               </div>
+
+              <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Conta {dado.account_number}</DialogTitle>
+                <DialogContent>
+                  <p>Agencia: {dado.agency}</p>
+                  <p>Saldo: {dado.balance.$numberDecimal}</p>
+
+                  <p>User: {dado.user_id}</p>
+                  <p>Data de Criação : {dado.created_at}</p>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Fechar
+                  </Button>
+                </DialogActions>
+              </Dialog>
             </CardContent>
-            <Divider style={{ background: 'black'}} variant="middle" />
+            <Divider style={{ background: 'black' }} variant="middle" />
           </Card>
         ))}
       </ul>
--- frontend/src/components/ListApplication/index.tsx
+++ frontend/src/components/ListApplication/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button, Card, CardContent, Divider, Chip, Grid } from "@mui/material";
+import { Button, Card, CardContent, Divider, Chip, Grid, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 import IApp from "../../interfaces/IApp";
 import styles from "./ListApplication.module.scss";
 
@@ -10,6 +10,16 @@ const url = 'https://moneyapp.onrender.com/application';
 const ListApplication = () => {
 
     const [applications, setApplications] = useState<IApp[]>([]);
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     useEffect(() => {
         axios
@@ -33,55 +43,71 @@ const ListApplication = () => {
                     <h2 className="mt-2"> {item.type}</h2>
                     <h3> {item.application} </h3>
                     <Grid container spacing={4}>
-                    <CardContent>
-                        <div className={styles.main} >
-                         <Chip label="Account ID:" color="primary" variant="outlined" /> <h4>{item.account_id}</h4>  
-                                
-                            <Chip label="Moeda:" color="success"variant="outlined"  />
-                            <h4>
-                             {item.currency} 
+                        <CardContent>
+                            <div className={styles.main} >
+                                <Chip label="Account ID:" color="primary" variant="outlined" /> <h4>{item.account_id}</h4>
 
-                            </h4>
-                             <Chip label="Valor:" color="primary" variant="outlined" />
-                             <h4>
-                             {item.value?.$numberDecimal}
+                                <Chip label="Moeda:" color="success" variant="outlined" />
+                                <h4>
+                                    {item.currency}
 
-                             </h4>
-                             <Chip label="Vencimento:" color="primary" variant="outlined" />
-                             <h4>
-                              {item.expiration || "FALSE"} 
+                                </h4>
+                                <Chip label="Valor:" color="primary" variant="outlined" />
+                                <h4>
+                                    {item.value?.$numberDecimal}
 
-                             </h4>
+                                </h4>
+                                <Chip label="Vencimento:" color="primary" variant="outlined" />
+                                <h4>
+                                    {item.expiration || "FALSE"}
 
-                             <Chip label="Data de Aplicação:" color="primary"  variant="outlined"/>
-                             <h4>
-                              {item.applicated_at} 
-
-                             </h4>
-                             <Chip label="Data de Vencimento:" color="primary" variant="outlined" />
-                             
-                             <h4>
-                                 {item.expiration_date}
                                 </h4>
 
-                             <Chip label="Resgatado:" color="primary" variant="outlined" />
-                             
-                             <h4>
-                             {item.flag_redemption || "FALSE"}
+                                <Chip label="Data de Aplicação:" color="primary" variant="outlined" />
+                                <h4>
+                                    {item.applicated_at}
 
-                             </h4>
-                            
+                                </h4>
+                                <Chip label="Data de Vencimento:" color="primary" variant="outlined" />
 
-                        </div>
-                        <li><Button className="btn-form" color="warning" variant="contained">Editar</Button></li>
-                            <li><Button color="error" variant="contained">Excluir</Button></li>
+                                <h4>
+                                    {item.expiration_date}
+                                </h4>
 
-                    </CardContent>
-            </Grid>
-                    <Divider style={{ background: 'black'}} variant="middle" />
+                                <Chip label="Resgatado:" color="primary" variant="outlined" />
+
+                                <h4>
+                                    {item.flag_redemption || "FALSE"}
+
+                                </h4>
+
+
+                            </div>
+                            <li><Button className="btn-form" color="warning" variant="contained" onClick={handleOpen}>Detalhes</Button></li>
+  
+                            <Dialog open={open} onClose={handleClose}>
+                                <DialogTitle>Aplicação {item.currency} </DialogTitle>
+                                <DialogContent>
+                                    <p>ID: {item._id}</p>
+                                    <p>Valor: {item.value?.$numberDecimal}</p>
+
+                                    <p>Data de Vencimento: {item.expiration_date}</p>
+                                    <p>Data de Registro: {item.applicated_at}</p>
+
+                                 </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleClose} color="primary">
+                                        Fechar
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
+
+                        </CardContent>
+                    </Grid>
+                    <Divider style={{ background: 'black' }} variant="middle" />
 
                 </Card>
-                
+
             ))}
         </>
     )
