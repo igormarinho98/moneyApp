4c73451e5a44a5e1f554b02613cf7c3b77370a57
Igor Marinho <igormarinholeo2014@gmail.com>
2023-09-09 10:55:16 -0300

    Navbar Updated

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,48 +1,30 @@
-import {  Breadcrumbs,Link, Alert } from "@mui/material";
-
-
+import React from "react";
+import { Breadcrumbs, Link, Alert } from "@mui/material";
+import styles from "./Navbar.module.scss"; // Importe os estilos SASS
 
 const NavBar = () => {
-
-    return (
-        <>
-         <Alert variant="filled" severity="success">
-          WEBvest
-        </Alert>
-
-        <Breadcrumbs aria-label="Menu">
-  <Link underline="hover" color="inherit" href="/">
-    Início
-  </Link>
-  <Link
-    underline="hover"
-    color="inherit"
-    href="/account"
-    >
-    Gestão de Contas
-  </Link>
-
-  <Link
-    underline="hover"
-    color="inherit"
-    href="/user"
-    >
-    Gestão de Usuários
-  </Link>
-  <Link
-    underline="hover"
-    color="inherit"
-    href="/application"
-    >
-    Aplicações
-  </Link>
-</Breadcrumbs>
-<h2>Sistema de Conta Corrente</h2>
-
-        </>
-);
+  return (
+    <div className={styles.navbar}>
+      <Alert variant="filled" severity="success">
+        WEBvest
+      </Alert>
+      <Breadcrumbs aria-label="Menu" className={styles["navbar-links"]}>
+        <Link underline="hover" color="inherit" href="/">
+          Início
+        </Link>
+        <Link underline="hover" color="inherit" href="/account">
+          Gestão de Contas
+        </Link>
+        <Link underline="hover" color="inherit" href="/user">
+          Gestão de Usuários
+        </Link>
+        <Link underline="hover" color="inherit" href="/application">
+          Aplicações
+        </Link>
+      </Breadcrumbs>
+      <h2 className={styles["navbar-title"]}>Sistema de Conta Corrente</h2>
+    </div>
+  );
 }
-
-
 
 export default NavBar;
